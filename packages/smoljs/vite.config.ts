cc40486785b873df0dc2e3c45ce06f77a53d9d24
Andrzej Kurek <32792205+Razz21@users.noreply.github.com>
2025-01-27 21:05:09 +0100

    feat: configure prepack

--- packages/smoljs/vite.config.ts
+++ packages/smoljs/vite.config.ts
@@ -1,24 +1,25 @@
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import pkg from './package.json';
 
 export default defineConfig({
   build: {
     target: 'esnext',
     lib: {
       entry: [resolve(__dirname, 'src/index.ts'), resolve(__dirname, 'src/jsx-runtime.ts')],
-      name: 'Smoljs',
+      name: pkg.name,
       fileName: (format, entryName) => `${entryName}.${format === 'es' ? 'js' : 'min.js'}`,
     },
 
     rollupOptions: {
       preserveEntrySignatures: 'allow-extension',
-      external: ['@smoljs/runtime'],
-      output: {
-        globals: {
-          '@smoljs/runtime': 'SmoljsRuntime',
-        },
-      },
+      // external: ['@smoljs/runtime'],
+      // output: {
+      //   globals: {
+      //     '@smoljs/runtime': 'SmoljsRuntime',
+      //   },
+      // },
     },
   },
 
