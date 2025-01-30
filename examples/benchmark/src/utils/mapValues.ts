a9600eb9183d85b6d9b11a18544435c5cc3b5e76
Andrzej Kurek <32792205+Razz21@users.noreply.github.com>
2025-01-30 19:19:36 +0100

    chore: examples cleanup

--- examples/benchmark/src/utils/mapValues.ts
+++
@@ -1,8 +0,0 @@
-export function mapValues<T extends object, R>(obj: T, iteratee: (value: any, key: string) => R): { [K in keyof T]: R } {
-  const keys = Object.keys(obj) as (keyof T & string)[];
-  const result = keys.reduce((acc, key) => {
-    acc[key] = iteratee(obj[key], key);
-    return acc;
-  }, {} as { [K in keyof T]: R });
-  return result;
-}
